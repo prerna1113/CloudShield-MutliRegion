a01a1c93f7d8538e376df74216386ba5be786964
Prerna Dubey <prernakumari7427@gmail.com>
2026-06-30 22:17:30 +0530

    cloudShield Inital commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,19 +2,24 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"Prerna@9211",
-    database:"cloudshield"
+    host:"database-1.c5uoauwmqc7u.ap-south-1.rds.amazonaws.com",
+    user:"admin",
+    password:"admin123",
+    database:"cloudshield",
+    port:3306
 
 });
 
-db.connect((err)=>{
-    if(err){
-        console.log(err);
-    } else{
-        console.log("Database connected")
-    };
-});
+const connectDB = () => {
+  db.connect((err) => {
+    if (err) {
+      console.log("DB not ready, retrying...");
+      setTimeout(connectDB, 5000);
+    } else {
+      console.log("Database Connected");
+    }
+  });
+};
 
+connectDB();
 module.exports = db;
